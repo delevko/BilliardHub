d56ba5e734e74b0308c979bd50c3deb565e8d5ca
delevko <delevkoo@gmail.com>
2019-11-12 05:19:19 -0700

    divide standby into two lobbies

---
+++ templates/admin/tournaments/navigations/standbyRounds.php
@@ -0,0 +1,14 @@
+<?php $header = "lobby.php?id=$tournamentID"; ?>
+
+<div class="tour_menu_box">
+	<nav class="tour_menu">
+		<a href="<?=$header?>&onClick=rounds" id="rounds">
+			Раунди Очки
+		</a>
+		
+		<a href="<?=$header?>&onClick=participants" id="participants">
+			Учасники
+		</a>
+	</nav>
+	<div class="margin-b_30"></div>
+</div>
