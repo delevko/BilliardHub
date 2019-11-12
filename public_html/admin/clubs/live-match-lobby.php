f198e76cfe6287f0f2259e86c0cd4bf05db64075
delevko <delevkoo@gmail.com>
2019-11-12 13:57:33 -0700

    update matchHeader display

--- public_html/admin/clubs/live-match-lobby.php
+++ public_html/admin/clubs/live-match-lobby.php
@@ -16,9 +16,9 @@ $query = "SELECT
     M.player1Score, M.player2Score, M.bestOf,
     T.name AS tournamentName,
     LM.points1, LM.points2, LM.break1, LM.break2,
-    M.roundType, M.roundNo, M.groupID,
+    M.roundType,
     X.photo AS photo1, Y.photo AS photo2,
-    C.photo AS clubPhoto
+    C.photo AS clubPhoto, M.id
 FROM _table tbl
     LEFT JOIN _match M ON tbl.matchID = M.id
     LEFT JOIN tournament T ON M.tournamentID = T.id
@@ -30,46 +30,58 @@ FROM _table tbl
 WHERE tbl.id=?";
 
 $data = query($query, $tableID);
-$tableNum = $data[0][0]; $clubName = $data[0][1];
-$club_img = $data[0][20];
 
-$tableStatus = $data[0][2]; $matchStatus = $data[0][3]; $matchNum = $data[0][4];
-$player1 = $data[0][5]; $player2 = $data[0][6]; $bestOf = $data[0][9];
+$tableNum = $data[0][0]; $clubName = $data[0][1];
+$club_img = $data[0][18];
+
+$tableStatus = $data[0][2]; $matchStatus = $data[0][3];
+$matchNum = $data[0][4]; $matchID = $data[0][19];
+$roundType = $data[0][15];
+
+$player1 = $data[0][5]; $player2 = $data[0][6];
+$img1 = $data[0][16]; $img2 = $data[0][17];
+
+$bestOf = $data[0][9];
 $score1 = $data[0][7]; $score2 = $data[0][8]; 
-$points1 = $data[0][11]; $points2 = $data[0][12]; $break1 = $data[0][13]; $break2 = $data[0][14];
-$roundType = $data[0][15]; $roundNo = $data[0][16]; $groupID = $data[0][17];
-$img1 = $data[0][18]; $img2 = $data[0][19];
+$points1 = $data[0][11]; $points2 = $data[0][12];
+$break1 = $data[0][13]; $break2 = $data[0][14];
+
 
 $player1class = "live-match-lobby-player"; $player2class = "live-match-lobby-player";
 ( nonEmpty($break1) ) ? ($player1class .= " highlight") : ($player2class .= " highlight");
 
-$matchInfo = matchInfo($roundType, $roundNo, $groupID);
+$matchInfo = matchInfo($matchID, $roundType);
 
-if( !strcmp($tableStatus, "Occupied") ) { 
-	if( !strcmp($matchStatus, "Live") ){
+
+if( !strcmp($tableStatus, "Occupied") )
+{ 
+	if( !strcmp($matchStatus, "Live") )
 		require("live_match.html");
-	}
-	else if( !strcmp($matchStatus, "Finished") ){
+	else if( !strcmp($matchStatus, "Finished") )
 		require("finished_match.html");
-	}
 }
-else {
+else
+{
 	redirect("tableLobby.php?id=$tableID");
 }
+
 
 //liveSparringLobby finishedSparringLobby
 //if tableStatus="available" -> availableSparringLobby
 
-function matchInfo($roundType, $roundNo, $groupID){
-	if( !strcmp($roundType, "Group") ){
-		$data = query("SELECT G.groupNum FROM groupTournament G WHERE G.id=?", $groupID);
-		$roundNo = $data[0][0];
-	}
+function matchInfo($id, $rType)
+{
+    $grpORround = ($rType=="Group") ? "GT.groupNum" : "M.roundNo";
+    $query = "SELECT ".$grpORround.", T.KO_Rounds, T.seeded_Round
+   FROM _match M JOIN tournament T ON M.tournamentID=T.id
+   LEFT JOIN groupTournament GT ON M.groupID = GT.id
+   WHERE M.id=?";
+    $data = query($query, $id);
+    $rndNo = $data[0][0]; $KO_R = $data[0][1];
+    $seeded_R = $data[0][2];
 
-	$info = castMatchHeader($roundType).$roundNo;
-
-
-	return $info;
+    $header = castHeader($rType, $rndNo, $KO_R, $seeded_R);
+    return $header;
 }
 
 ?>
