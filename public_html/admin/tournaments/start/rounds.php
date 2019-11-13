a3f4aa5881e018969602168aaa70a7bca5fc3a74
delevko <delevkoo@gmail.com>
2019-11-13 14:30:11 -0700

    finalize tournament details input

--- public_html/admin/tournaments/start/rounds.php
+++ public_html/admin/tournaments/start/rounds.php
@@ -4,17 +4,108 @@ require("../../../../includes/adminConfig.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-	list($id) = checkData($_POST["id"]);
+	list($id, $bracket) = checkData($_POST["id"]);
 
-//	list($N, $registered, $add_R, $seeded_R, $KO_R) = getData($seeded, $id);
+	$minBreak = "";
+	if( !nonEmpty($_POST["minBreak"]) )
+		$minBreak = 20;
+	else
+		$minBreak = $_POST["minBreak"];
 
-//	$query = "CALL KnockoutGenerate(?,?,?,?,?,?,?,?)";
-//	query($query, $id, $seeded, $KO_R, $add_R, $seeded_R, $N, $seeding, $registered);
+	list($LOW_R, $UP_R, $KO_R) = getDEData($id);
 
-//	seedPlayers($id, $N/2, $seeded_R);
+	$groups_P = ""; $lows_P = ""; $kos_P = "";
+	$lows_P .= "$LOW_R:"; $kos_P .= ($KO_R+1).":";
 
-	$query = "CALL startTournament(?)";
-	query($query, $id);
+	$lows_B .= "$LOW_R:"; $ups_B .= "$UP_R:";
+	$kos_B .= "$KO_R:"; $groups_B = "";
+
+	if($bracket == "GroupKO")
+	{
+    		list($grpMax, $grpProceed) = getGroupData($id);
+		$groups_P .= ($grpMax-$grpProceed).":";
+		
+		for($i = $grpMax; $i > $grpProceed; $i--)
+		{
+			if( !nonEmpty($_POST["Group_R-$i"]) )
+			{
+				adminApology(INPUT_ERROR,
+				"Введіть дані для всіх полів");
+				exit;
+			}
+			$rnd_p = $_POST["Group_R-$i"];
+			$groups_P .= "$rnd_p,";
+		}
+		if( !nonEmpty($_POST["Group_B"]) )
+		{
+			adminApology(INPUT_ERROR,
+			"Введіть дані для всіх полів");
+			exit;
+		}
+		$groups_B = $_POST["Group_B"];
+	}
+
+
+	for($i = 1; $i <= $LOW_R; $i++)
+	{
+		if( !nonEmpty($_POST["LOW_R-$i"]) ||
+		!nonEmpty($_POST["LOW_B-$i"]) )
+		{
+			adminApology(INPUT_ERROR,
+			"Введіть дані для всіх полів");
+			exit;
+		}
+		$rnd_p = $_POST["LOW_R-$i"];
+		$lows_P .= "$rnd_p,";
+
+		$rnd_p = $_POST["LOW_B-$i"];
+		$lows_B .= "$rnd_p,";
+	}
+
+	
+        for($i = 1; $i <= $UP_R; $i++)
+	{
+		if( !nonEmpty($_POST["UP_B-$i"]) )
+		{
+			adminApology(INPUT_ERROR,
+			"Введіть дані для всіх полів");
+			exit;
+		}
+		$rnd_p = $_POST["UP_B-$i"];
+		$ups_B .= "$rnd_p,";
+	}
+
+
+	for($i = 1; $i <= $KO_R; $i++)
+	{
+		if( !nonEmpty($_POST["KO_R-$i"]) ||
+		!nonEmpty($_POST["KO_B-$i"]) )
+		{
+			adminApology(INPUT_ERROR,
+			"Введіть дані для всіх полів");
+			exit;
+		}
+		$rnd_p = $_POST["KO_R-$i"];
+		$kos_P .= "$rnd_p,";
+
+		$rnd_p = $_POST["KO_B-$i"];
+		$kos_B .= "$rnd_p,";
+	}
+
+	$champ = $KO_R+1;
+	if( !nonEmpty($_POST["KO_R-$champ"]) )
+	{
+		adminApology(INPUT_ERROR,
+		"Введіть дані для всіх полів");
+		exit;
+	}
+	$rnd_p = $_POST["KO_R-$champ"];
+	$kos_P .= "$rnd_p,";
+
+
+	$query = "CALL tournamentInput(?,?,?,?,?,?,?,?,?)";
+	query($query, $id, $minBreak, $groups_P, $lows_P, $kos_P,
+		$groups_B, $lows_B, $ups_B, $kos_B);
 
 	redirect("../lobby.php?id=".$id."&onClick=default");
 }
@@ -24,79 +115,44 @@ else
 }
 
 
-function seedPlayers($id, $seeded, $seeded_R)
-{
-	if($seeded_R > 1)
-		$seed_array = getSeedingArrayMy(LOG($seeded*2, 2), 1, $seeded*2);
-	else
-		$seed_array = getSeedingArray(LOG($seeded*2, 2), 1, $seeded*2);
-
-	$flag = 0;
-	for($i = $seeded_R; $i >= 1; $i--)
-	{
-		$match_counter = ($i-1)*$seeded + 1;
-		for($j = 0; $j < $seeded; $j++)
-		{
-			$playerSeed = $seed_array[$j*2+$flag] + ($seeded_R-$i-$flag)*$seeded;
-			query("CALL seedPlayer(?,?,?,?)", $id, $match_counter, "K/O", $playerSeed);
-			$match_counter++;
-		}
-		if($flag === 0)
-			$flag = 1;
-		else 
-			$flag = 0;
-	}
-
-	$match_counter = 1;
-	for($j = 0; $j < $seeded; $j++)
-	{
-		$playerSeed = $seed_array[$j*2+$flag] + ($seeded_R-$flag)*$seeded;
-		query("CALL seedPlayer(?,?,?,?)", $id, $match_counter, "K/O", $playerSeed);
-		$match_counter++;
-	}
-}
-
 
 function checkData($id)
 {
 	if( !nonEmpty($id) || !exists("tournament", $id) )
 		redirect(PATH_H."logout.php");
+
+	$query = "SELECT T.bracket FROM tournament T WHERE T.id=?";
+	$data = query($query, $id);
 	
-	return array($id);
+	return array($id, $data[0][0]);
 }
 
-function getPlayers($id)
+
+function getDEData($id)
 {
-	$data = query("SELECT registeredPlayers FROM tournament WHERE id = ?", $id);
-	return $data[0][0];
+    $query = "SELECT T.LOW_Rounds, T.UP_Rounds, T.KO_Rounds
+        FROM tournament T WHERE T.id=?";
+    $data = query($query, $id);
+
+    $LOW_R = $data[0][0]; $UP_R = $data[0][1];
+    $KO_R = $data[0][2];
+
+    return array($LOW_R, $UP_R, $KO_R);
 }
 
-function getData($seeded, $id)
-{
-	$registered = getPlayers($id);
-	
-	if( $seeded > $registered )
-	{
-		adminApology(INPUT_ERROR, "Кількість сіяних гравців не може перевищувати кількість учасників турніру");
-		exit;
-	}
-	
-	if( $seeded === 0 )
-	{	
-		$N = firstGreaterPowerOf2($registered);
-		$add_R = 0;
-		$seeded_R = 1;
-		$KO_R = LOG($N, 2);
-	}
-	else
-	{
-		$N = $seeded*2;
-		$registered -= $seeded;
-		$add_R = FLOOR( ($registered-1)/$seeded );
-		$seeded_R = $add_R + 1;
-		$KO_R = $add_R + LOG($N, 2);
-	}
 
-	return array($N, $registered, $add_R, $seeded_R, $KO_R);
+function getGroupData($id)
+{
+    $query = "SELECT T.groupMin, T.groupPlayers,
+	T.nrOfGroups, T.groupProceed
+        FROM tournament T WHERE T.id=?";
+    $data = query($query, $id);
+
+    $grpMin = $data[0][0]; $grpPlrs = $data[0][1];
+    $n_grps = $data[0][2]; $grpProceed = $data[0][3];
+
+    $grpMax = $grpMin + ceil( ($grpPlrs % $grpMin) / $n_grps );
+
+    return array($grpMax,$grpProceed);
 }
 ?>
