4812c5ef922eaa9510c8533b3c5e07f4d8e48e83
delevko <delevkoo@gmail.com>
2019-11-13 15:18:55 -0700

    add admin tournament navigation mobile version

--- templates/admin/tournaments/navigations/standbyBracket.php
+++ templates/admin/tournaments/navigations/standbyBracket.php
@@ -1,7 +1,11 @@
 <?php $header = "lobby.php?id=$tournamentID"; ?>
 
 <div class="tour_menu_box">
-	<nav class="tour_menu">
+	<nav class="tour_menu" id="tournament_navigation">
+                <a class="icon" onclick="mobileTournamentNav()">
+                 <i class="fa fa-bars"></i>&nbsp;Турнірне меню
+                 </a>
+
 		<a href="<?=$header?>&onClick=KO" id="KO">
 			KNOCKOUT
 		</a>
