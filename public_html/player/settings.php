a4bc51b9ce93bced44e593a882c47c3fe61da565
delevko <delevkoo@gmail.com>
2019-11-14 07:45:28 -0700

    update settings for player

--- public_html/player/settings.php
+++ public_html/player/settings.php
@@ -7,7 +7,7 @@ if( isset($_SESSION["id"] ) )
 {
     if( !loginAvailable($_SESSION["id"]["login"]) )
     {
-	$title = "Налаштування";
+	$title = "Налаштування ".$_SESSION["id"]["login"];
 
 
         render("player/settings.php", ["title"=>$title]);
