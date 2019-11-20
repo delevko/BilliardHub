3e1e77b03ee397dc3bbb4b6017df61c96c6863ab
delevko <delevkoo@gmail.com>
2019-11-20 12:21:19 -0700

    change nbsp to margin

--- templates/admin/tournaments/navigations/header.php
+++ templates/admin/tournaments/navigations/header.php
@@ -4,6 +4,6 @@
 <div class="tour_menu_box">
 	<nav class="tour_menu" id="tournament_navigation">
 		<a class="icon" onclick="mobileTournamentNav()">
-                 <i class="fa fa-bars"></i>&nbsp;Турнірне меню
+                 <i class="fa fa-bars"></i>Турнірне меню
                  </a>
 
