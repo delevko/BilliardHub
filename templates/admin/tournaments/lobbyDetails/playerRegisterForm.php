44fb8e1ae2432694dd443cfe9ec2dedf464e6812
Danylo Sokolov <gagunets@gmail.com>
2019-11-21 00:17:18 +0200

    select&register player

--- templates/admin/tournaments/lobbyDetails/playerRegisterForm.php
+++ templates/admin/tournaments/lobbyDetails/playerRegisterForm.php
@@ -19,22 +19,24 @@ if(count($data))
 {
 ?>
 	<div class="margin-b_30"></div>
-	<form class="player_admin_reg" action="playerRegister.php" method="post">
-		<select name="player">
-			<?php
-				
-			for($i=0; $i<count($data); $i++)
-			{
-				$playerID = $data[$i][0];
-				$playerName = $data[$i][1];
-?>
-				<option value="<?=$playerID?>"><?=$playerName?></option>
-	  <?php } ?>
-		</select>
-		<input type="hidden" name="tournament" value="<?=$tournamentID?>">
-		<div class="margin-b_30"></div>
-	</form>
-	<button class="player_admin_reg_button" type="submit">Зареєструвати гравця</button>
+	<div class="player_admin_reg_box">
+		<form class="player_admin_reg" action="playerRegister.php" method="post">
+			<select name="player">
+				<?php
+					
+				for($i=0; $i<count($data); $i++)
+				{
+					$playerID = $data[$i][0];
+					$playerName = $data[$i][1];
+	?>
+					<option value="<?=$playerID?>"><?=$playerName?></option>
+		  <?php } ?>
+			</select>
+			<input type="hidden" name="tournament" value="<?=$tournamentID?>">
+			<div class="margin-b_30"></div>
+		</form>
+		<button class="player_admin_reg_button" type="submit">Зареєструвати гравця</button>
+	</div>
 <?php
 } ?>
 
