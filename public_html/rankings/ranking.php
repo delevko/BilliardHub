dac669c697ae0374038ac6c189da9a4eff60e34e
delevko <delevkoo@gmail.com>
2019-11-29 08:12:59 -0700

    fix empty id bug

--- public_html/rankings/ranking.php
+++ public_html/rankings/ranking.php
@@ -4,10 +4,9 @@ require("../../includes/config.php");
 
 if($_SERVER["REQUEST_METHOD"] == "GET")
 {
-	$leagueID = $_GET["id"];
-	if( !exists("league", $leagueID) )
+	$leagueID = isset($_GET["id"]) ? htmlspecialchars($_GET["id"]) : null;
+	if( !nonEmpty($leagueID) || !exists("league", $leagueID) )
 		redirect("");
-
 
 
 	render("rankings/ranking.php", ["leagueID"=>$leagueID, "title"=>"Рейтинг"]);
