f2f72b7660cc2f6cd153bbc556a2d2beca1303da
delevko <delevkoo@gmail.com>
2019-12-10 08:33:40 -0700

    change order of nav menu

--- templates/admin/tournaments/navigations/registration.php
+++ templates/admin/tournaments/navigations/registration.php
@@ -1,9 +1,9 @@
 
-		<a href="<?=$header?>&onClick=participants" id="participants">
-			Учасники
-		</a>
 		<a href="<?=$header?>&onClick=description" id="description">
 			Регламент
+		</a>
+		<a href="<?=$header?>&onClick=participants" id="participants">
+			Учасники
 		</a>
 		<a href="<?=$header?>&onClick=register" id="register">
 			Зареєструвати
