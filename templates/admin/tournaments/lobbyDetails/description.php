5ad4a62062fe6bbcea9f816237a7a05e4542767e
Danylo Sokolov <gagunets@gmail.com>
2019-12-11 19:30:02 +0200

    reglament styling

--- templates/admin/tournaments/lobbyDetails/description.php
+++ templates/admin/tournaments/lobbyDetails/description.php
@@ -15,9 +15,9 @@
 function displayDescription($description, $tournamentID)
 { ?>
 	<form class="form_reglament" method="POST" action="<?=PATH_H?>admin/tournaments/changeDescription.php">
-		<p placeholder="Введіть текст.." class="reglament" type="text" maxlength="60000"
+		<textarea placeholder="Введіть текст.." class="reglament" type="text" maxlength="60000"
 		name="description"><?=$description?>
-        </p>
+        </textarea>
 		<input type="hidden" name="tournamentID" value="<?=$tournamentID?>">
 		<button class="b_reglament">
 			Змінити
