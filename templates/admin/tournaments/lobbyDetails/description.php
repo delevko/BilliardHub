37b9bba6f17526d84e79132503113ffcf9548a9c
Danylo Sokolov <gagunets@gmail.com>
2019-12-11 20:15:31 +0200

    reglament styling

--- templates/admin/tournaments/lobbyDetails/description.php
+++ templates/admin/tournaments/lobbyDetails/description.php
@@ -22,21 +22,7 @@ function displayDescription($description, $tournamentID)
 			Змінити
 		</button>
 	</form>
-
-    <script>
-        
-        var textarea = document.querySelector('textarea');
-
-textarea.addEventListener('keydown', autosize);
-             
-function autosize(){
-  var el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-  },0);
-}
-    </script>
+    
 <?php }
 
 function displayHeader()
