a33814041625d2fd990c52be3e8c7c5c95540dd4
delevko <delevkoo@gmail.com>
2019-12-13 18:46:55 -0700

    add resetBracket and delete tournament procedures

--- database/tournamentProcedures.sql
+++ database/tournamentProcedures.sql
@@ -6,6 +6,9 @@ DROP TRIGGER IF EXISTS finishTournament;
 
 DROP PROCEDURE IF EXISTS getFirstGreaterPowerOf2;
 
+DROP PROCEDURE IF EXISTS resetBracket;
+DROP PROCEDURE IF EXISTS deleteTournament;
+
 delimiter $$
 
 
@@ -37,9 +40,92 @@ BEGIN
     -- update rating for each player
         UPDATE rating R INNER JOIN tournamentStandings TS ON R.playerID=TS.playerID
         SET R.points = R.points+TS.points WHERE R.leagueID=NEW.leagueID AND TS.tournamentID=NEW.id;
+
+    -- reset table youtubes, where this tournament took part in
+	UPDATE _table T set T.youtube = NULL WHERE T.clubID = NEW.clubID;
     END IF;
 END;
 
+
+-- resets tournament to standby phase
+CREATE PROCEDURE resetBracket(IN tournamentID INT)
+BEGIN
+START TRANSACTION;
+-- match data
+    DELETE B FROM break B WHERE B.tournamentID = tournamentID;
+
+    DELETE LM FROM liveMatch LM WHERE LM.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE F FROM frame F WHERE F.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE MD FROM matchDetails MD WHERE MD.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE M FROM _match M WHERE M.tournamentID = tournamentID;
+
+-- groups data
+    DELETE GS FROM groupStandings GS WHERE GS.tournamentID = tournamentID;    
+
+    DELETE GS FROM groupSeeding GS WHERE GS.tournamentID = tournamentID;    
+
+    DELETE PG FROM playerGroup PG WHERE PG.groupID IN
+	(SELECT GT.id FROM groupTournament GT WHERE GT.tournamentID = tournamentID);
+
+    DELETE GT FROM groupTournament GT WHERE GT.tournamentID = tournamentID;
+
+
+-- general tournament data
+    DELETE TP FROM tournamentPoints TP WHERE TP.tournamentID = tournamentID;
+
+    UPDATE tournament T SET T.bracket = NULL, T.status = "Standby" WHERE T.id = tournamentID;
+COMMIT;
+END;
+
+
+
+-- deletes all tournament data
+CREATE PROCEDURE deleteTournament(IN tournamentID INT)
+BEGIN
+START TRANSACTION;
+-- match data
+    DELETE B FROM break B WHERE B.tournamentID = tournamentID;
+
+    DELETE LM FROM liveMatch LM WHERE LM.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE F FROM frame F WHERE F.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE MD FROM matchDetails MD WHERE MD.matchID IN
+	(SELECT M.id FROM _match M WHERE M.tournamentID=tournamentID);
+
+    DELETE M FROM _match M WHERE M.tournamentID = tournamentID;
+
+-- groups data
+    DELETE GS FROM groupStandings GS WHERE GS.tournamentID = tournamentID;    
+
+    DELETE GS FROM groupSeeding GS WHERE GS.tournamentID = tournamentID;    
+
+    DELETE PG FROM playerGroup PG WHERE PG.groupID IN
+	(SELECT GT.id FROM groupTournament GT WHERE GT.tournamentID = tournamentID);
+
+    DELETE GT FROM groupTournament GT WHERE GT.tournamentID = tournamentID;
+
+
+-- general tournament data
+    DELETE TP FROM tournamentPoints TP WHERE TP.tournamentID = tournamentID;
+
+    DELETE TS FROM tournamentStandings TS WHERE TS.tournamentID = tournamentID;
+
+    DELETE PT FROM playerTournament PT WHERE PT.tournamentID = tournamentID;
+
+    DELETE TD FROM tournament_details TD WHERE TD.tournamentID = tournamentID;
+
+    DELETE T FROM tournament T WHERE T.id = tournamentID;
+COMMIT;
+END;
 
 
 
