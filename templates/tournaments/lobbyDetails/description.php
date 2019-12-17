ae303217925caaa7400dc19f503d44624ba81fd6
Danylo Sokolov <gagunets@gmail.com>
2019-12-17 12:56:00 +0200

    reglament

--- templates/tournaments/lobbyDetails/description.php
+++ templates/tournaments/lobbyDetails/description.php
@@ -17,8 +17,8 @@ function displayDescription($description)
 
    <!--  <textarea readonly  class="reglament_text"><?=$description?></textarea>  -->
     <div class="reglament_box">
-      <p class="reglament_text"><?=$description?>
-      </p> 
+      <div class="reglament_text"><?=$description?>
+      </div> 
     </div>
 
 <?php }
