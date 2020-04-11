3910b03c8d3c21ce6682e8d78c59ca01e4d81e28
delevko <delevkoo@gmail.com>
2020-04-11 04:34:03 -0700

    manual description change for live tournament

---
+++ public_html/admin/description.php
@@ -0,0 +1,49 @@
+
+<?php
+
+require("../../includes/adminConfig.php");
+?>
+
+<link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/admin_description.css">
+
+<?php
+    $tournamentID=32;
+    $query = "SELECT TD.description FROM tournament_details TD
+	WHERE TD.tournamentID=?";
+    $data = query($query, $tournamentID);
+
+
+    displayHeader();
+
+    displayDescription($data[0][0], $tournamentID);
+
+    displayFooter();
+
+function displayDescription($description, $tournamentID)
+{ ?>
+	<form class="form_reglament" method="POST" action="<?=PATH_H?>admin/tournaments/changeDescription.php">
+		<textarea placeholder="Введіть текст.." class="reglament" maxlength="60000"
+		name="description" wrap="soft"><?=$description?></textarea>
+		<input type="hidden" name="tournamentID" value="<?=$tournamentID?>">
+		<button class="b_reglament">
+			Змінити
+		</button>
+	</form>
+
+<?php }
+
+function displayHeader()
+{ ?>
+    <div class="section_header_700">
+        <div class="header_sign">
+            <i class="far fa-file-alt"></i>
+            Регламент
+        </div>
+    </div>
+<?php }
+
+function displayFooter()
+{ ?>
+<?php
+} ?>
+
